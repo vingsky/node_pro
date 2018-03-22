f1101ee1a29d5916e832bcb388317e4f6084cd15
wy <215723287@qq.com>
2018-03-23 01:17:55 +0800

    00

--- node/server.js
+++ node/server.js
@@ -4,6 +4,6 @@ http.createServer(function(req,res){
     console.log(req.query);
     console.log('====================================')
     res.writeHead(200,{'content-type':'text/plain;charset=utf-8','Access-Control-Allow-Origin':'*'});
-    res.write('魏延你好！');
+    res.write('魏延你好！@');
     res.end();
 }).listen(9999)
